ea48fe0666cf5a3620321cc55a108eb78e0acf6e
Jessicaenvisions <jessicaambrizmadrigal@gmail.com>
2025-03-29 08:37:49 -0700

    This is commit has modifications to the Basics file regarding what a variable is and comments on what will be disscussed in this file."
    q
    \
    \
    
    "

--- Basics.py
+++ Basics.py
@@ -1,5 +1,27 @@
-def say_hello(): 
-    print("Hello world")
+### This should include the basic python code that you want to run
+## Basics:
+    # Basics will cover the following: 
+    # Variables
+    # Data Types
+    # Basic Operators
 
 
-say_hello()
+# Variables:
+    # What are they? 
+        # Variables are used to store diffrenet data value types.
+    # This is an example of a variable:
+
+def variable_value(): 
+    x = 8
+    print("The variable x has a value of", x)
+    # But this variable can be changed to any value you want.
+    x = 17 
+    print("The variable x has a value of", x)
+variable_value()
+
+
+
+
+
+
+
